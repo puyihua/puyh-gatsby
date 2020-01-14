d8d6a0aa637b33c93f9cc40e96d24ff2160fae09
puyihua <puyihua1@gmail.com>
2020-01-14 14:35:00 +0800

    build the site skeleton; add posts page

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,13 +5,24 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: `puyh.pub`,
+  },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-transformer-remark`,
     `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
-        pathToConfigModule: `src/utils/typography`
-      }
-    }
-  ]
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
